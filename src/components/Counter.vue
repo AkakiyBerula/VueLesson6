db444cafad7e43507f3e87c70e9752e9bfade3c1
AkakiyBerula <vlad5dyakun.gmail.com>
2024-03-27 17:16:51 +0200

    10. Пагінація: (Виправлення)

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,24 +1,22 @@
 <template>
-  <v-app>
-    <v-container class="text-center">
-      <h1>{{ text }}</h1>
-      <h2>{{ counter }}</h2>
-      <v-btn @click="incrementTwo" color="success">+2</v-btn>
+  <v-container class="text-center">
+    <h1>{{ text }}</h1>
+    <h2>{{ counter }}</h2>
+    <v-btn @click="incrementTwo" color="success">+2</v-btn>
 
-      <v-btn @click="increment" color="success">+</v-btn>
+    <v-btn @click="increment" color="success">+</v-btn>
 
-      <v-btn @click="decrement" color="danger">-</v-btn>
+    <v-btn @click="decrement" color="danger">-</v-btn>
 
-      <v-btn @click="decrementTwo" color="danger">-2</v-btn>
+    <v-btn @click="decrementTwo" color="danger">-2</v-btn>
 
-      <h2> Квадрат числа: {{ getPow }} </h2>
+    <h2> Квадрат числа: {{ getPow }} </h2>
 
-      <h2> {{ isEven ? "Парне" : "Непарне" }}!</h2>
+    <h2> {{ isEven ? "Парне" : "Непарне" }}!</h2>
 
-      <v-text-field v-model="message" label="New Message"></v-text-field>
-      <v-btn @click="saveMessage" color="primary">Save Message</v-btn>
-    </v-container>
-  </v-app>
+    <v-text-field v-model="message" label="New Message"></v-text-field>
+    <v-btn @click="saveMessage" color="primary">Save Message</v-btn>
+  </v-container>
 </template>
 
 <script>
